61fa2baa096d9eefe6ef45474b8c8e2807f93610
pavelgrib <paul.quant@gmail.com>
2013-04-30 16:36:55 +0100

    new sims: qif, lif, hh;  stuff is more functional.... but broken

--- src/comp_neuro.py
+++ src/comp_neuro.py
@@ -4,9 +4,17 @@ Created on Apr 25, 2013
 
 @author: paul
 '''
-import math, cmath
+import math, cmath, operator
+from functools import partial
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+
+def mulTup(tup, times):
+    return tuple( map( lambda x: x*times, tup))
+
+def addTup(tup1, tup2):
+    return tuple(map(operator.add, tup1, tup2)) 
 
 class Graph(object):
     def __init__(self, links):
@@ -35,12 +43,7 @@ class Graph(object):
         return len( [x for x in self.links if ( node in x )] )
         
     def clusteringCoefficient(self, node, zeroConvention = 'Shanahan'):
-        def nonnode((a, b)):
-            if a == node:
-                return b
-            else:
-                return a
-        neighbors = [nonnode(x) for x in self.links if x[0] == node or x[1] == node]
+        neighbors = self.neighbors(node)
         d = len(neighbors) * (len(neighbors) - 1) / 2.0
         if len(neighbors) == 1:
             if zeroConvention == 'MATLAB':
@@ -50,6 +53,21 @@ class Graph(object):
         else:
             return len( [(i, j) for (i, j) in self.links if i in neighbors and j in neighbors] ) / d
     
+    def neighbors(self, ofNode):
+        def nonnode((a, b)):
+            if a == ofNode:
+                return b
+            else:
+                return a
+            return [nonnode(x) for x in self.links if 
+                    x[0] == ofNode or x[1] == ofNode]
+    
+    def pathLength(self, fromNode, toNode):
+        inf = float('inf')
+        dist = dict(zip(self.nodes, [inf]*len(self.nodes)))
+        dist[fromNode] = 0
+#         Q = self.nodes[:]   # creates a copy of the nodes
+#         prev = []
 
 def rhythmBand(hz):
     if hz >= 4 and hz <= 8:
@@ -94,17 +112,70 @@ def kuramotoSim(K, alpha, omega, N, maxT, dt):
             theta[t, i] = theta[t-1, i] + dt *(omega + sines.sum() / (N + 1))
     return theta
 
-def entropy(series):
-    """ 1/2 ln( (2pi * e)^N |COV_MATRIX(series)| """
-    pass
+def variance(series):
+    pd.Series(series).var()
 
-def izhikevichSim(v0, u0, I, a, b, c, d, N, dt):
+def covariance(S):
+    """ assuming that S is a set of time-series with T x N size """
+    N = S.shape[1]
+    covar = np.zeros(shape=(N, N))
+    for i in range(N):
+        iSeries = pd.Series(S[i])
+        for j in range(N):
+            covar[i,j] = iSeries.cov(pd.Series(S[j]))
+    return covar
+
+def entropy(S):
+    """ 1/2 ln( (2pi * e)^N |COV_MATRIX(S)| """
+    covDet = np.linalg.det(covariance(S))
+    return math.log(math.pow(2*math.pi*math.e, S.shape[1]) * covDet)
+
+def mutualInformation(S, i):
+    """ MI(S, i) = H(S-{X_i}) + H(X_i) - H(S) """
+    X = S[:,i]
+    return entropy(X) + entropy(S[:,range(len(X)).remove(i)])
+
+def integration(S):
+    """ I(S) = (sum H(X_i)) - H(S) """ 
+    isum = 0
+    for i in range(S.shape[1]):
+        isum += entropy(S[:,i])
+    return isum - entropy(S)
+    
+def complexity(S):
+    """ C(S) = (sum MI(S, i)) - I(S) """
+    csum = 0
+    for i in range(S.shape[1]):
+        csum += mutualInformation(S, i)
+    return csum - integration(S)
+    
+def izhikevichSim(v0, u0, I, a, b, c, d, N, dt, update='Euler'):
     v = np.zeros((N, 1))
     u = np.zeros((N, 1))
     v[0] = v0
     u[0] = u0
     
+    def izUpdate(recov, volts, current):
+        du =  a * ( b * volts - recov )
+        dv = 0.04*volts*volts + 5*volts + 140 - recov + current
+        return du, dv
+    
     for i in range(1, N):
+        k1u, k1v = izUpdate(u[i-1], v[i-1], I)
+        if update.lower()=='euler':
+            u[i] = u[i-1] + dt * k1u
+            v[i] = v[i-1] + dt * k1v
+        elif update.lower()=='rk' or update.lower()=='runge-kutta' or update.lower()=='rungekutta':
+            inputs = addTup( (u[i-1], v[i-1]), mulTup( (k1u, k1v), dt/2.0 ) )
+            k2u, k2v = izUpdate(inputs[0], inputs[1], I)
+            inputs = addTup( (u[i-1], v[i-1]), mulTup( (k2u, k2v), dt/2.0 ) )
+            k3u, k3v = izUpdate(inputs[0], inputs[1], I)
+            inputs = addTup( (u[i-1], v[i-1]), mulTup( (k3u, k3v), dt ) )
+            k4u, k4v = izUpdate(inputs[0], inputs[1], I)
+            v[i] = v[i-1] + dt*(k1v + 2*k2v + 2*k3v + k4v) / 6.0
+        else:
+            print 'update method ' + str(update) + ' not understood'
+            
         u[i] = u[i-1] + dt * ( a * ( b * v[i-1] - u[i-1] ) )
         v[i] = v[i-1] + dt * ( 0.04*v[i-1]*v[i-1] + 5*v[i-1] + 140 - u[i] + I )
         if v[i] > 30:
@@ -112,6 +183,111 @@ def izhikevichSim(v0, u0, I, a, b, c, d, N, dt):
             u[i] = u[i-1] + d
     return (u, v)
 
+def eulerUpdate(lastVal, updateFunc, dt):
+    return lastVal + updateFunc(lastVal) * dt
+
+def rkUpdate(lastVal, updateFunc, dt):
+    k1 = updateFunc(lastVal)
+    k2 = updateFunc(lastVal + k1 * dt / 2)
+    k3 = updateFunc(lastVal + k2 * dt / 2)
+    k4 = updateFunc(lastVal + k3 * dt    )
+    return lastVal + dt * (k1 + 2*k2 + 2*k3 + k4) / 6.0
+
+def hhSim(v0, m0, n0, h0, N, dt, I, update='Euler', C=1):
+    v = np.zeros((N,1), dtype=float)
+    m = float(m0)
+    n = float(n0)
+    h = float(h0)
+    v[0] = v0
+    
+    alpha_m = lambda volts: (2.5 - 0.1*volts) /(math.exp(2.5-0.1*volts) - 1)
+    alpha_n = lambda volts: (0.1 - 0.01*volts)/(math.exp(1.0-0.1*volts) - 1)    
+    alpha_h = lambda volts: 0.07  * math.exp(-volts/20.0)
+    beta_m  = lambda volts: 4.00  * math.exp(-volts/18)
+    beta_n  = lambda volts: 0.125 * math.exp(-volts/80)
+    beta_h  = lambda volts: 1.0   / (math.exp(3.0 - 0.1*volts)+1)
+    g = np.array([120, 36, 0.3])
+    E = np.array([115, -12, 10.6])
+    
+    def updateM(last_m, volts):
+        return alpha_m(volts)*(1-last_m) - beta_m(volts)*last_m
+    def updateN(last_n, volts):
+        return alpha_n(volts)*(1-last_n) - beta_n(volts)*last_n
+    def updateH(last_h, volts):
+        return alpha_h(volts)*(1-last_h) - beta_h(volts)*last_h
+    def hhUpdate(volts, last_m, last_n, last_h, curr): 
+        return curr + sum( g *(volts - E) * 
+                           np.array([last_h*math.pow(last_m,3), math.pow(last_n,4), 1]) )
+        
+    for i in range(1, N):
+        mUpdater = partial(updateM, volts=v[i-1])
+        nUpdater = partial(updateN, volts=v[i-1])
+        hUpdater = partial(updateH, volts=v[i-1])
+        vUpdater = partial(hhUpdate, last_m=m, last_n=n, last_h=h, curr=I)
+        if update.lower() == 'euler':
+            m    = eulerUpdate(m, mUpdater, dt)
+            n    = eulerUpdate(n, nUpdater, dt)
+            h    = eulerUpdate(h, hUpdater, dt)
+            v[i] = eulerUpdate(v[i-1], vUpdater, dt) / C
+        elif update.lower()=='rk' or update.lower()=='runge-kutta' or update.lower()=='rungekutta':
+            m    = rkUpdate(m, mUpdater, dt)
+            n    = rkUpdate(n, nUpdater, dt)
+            h    = rkUpdate(h, hUpdater, dt)
+            v[i] = rkUpdate(v[i-1], vUpdater, dt) / C
+        else: 
+            print 'update method ' + str(update) + ' not understood'
+    return v
+
+def LIFSim(v0, N, dt, I, update='Euler', R=1, tau=5, vr=-65, theta=-50, alpha=0):
+    v = np.zeros((N, 1), dtype=float)
+    v[0] = v0
+    t_spike = float('-inf')
+    
+    def lifUpdate(last_v):
+        return vr - last_v + R*I
+
+    for i in range(1, N):
+        if update.lower()=='euler':
+            temp = eulerUpdate( v[i-1], lifUpdate, dt) / tau
+        elif update.lower()=='rk' or update.lower()=='runge-kutta' or update.lower()=='rungekutta':
+            temp = rkUpdate(v[i-1], lifUpdate, dt) / tau
+        else:
+            print 'update method ' + str(update) + ' not understood'
+        if temp > theta and (i*dt - t_spike) > alpha:
+            v[i] = vr
+            t_spike = i*dt
+        else:
+            v[i] = temp
+    return v
+
+def QIFSim(v0, N, dt, I, update='Euler', R=1, a=0.04, tau=5, vr=-65, vc=-30, theta=-50, alpha=0):
+    v = np.zeros((N, 1), dtype=float)
+    v[0] = v0
+    t_spike = float('-inf')
+    def qifUpdate(last_v):
+        return a*(vr - last_v)*(vc - last_v) + R * I
+    
+    for i in range(1, N):
+        if update.lower()=='euler':
+            temp = eulerUpdate( v[i-1], qifUpdate, dt) / tau
+        elif update.lower()=='rk' or update.lower()=='runge-kutta' or update.lower()=='rungekutta':
+            temp = rkUpdate(v[i-1], qifUpdate, dt) / tau
+        else:
+            print 'update method ' + str(update) + ' not understood'
+        if temp > theta and (i*dt - t_spike) > alpha:
+            v[i] = vr
+            t_spike = i*dt
+        else:
+            v[i] = temp
+    return v
+     
+def numSpikes(v, threshold):
+    return len(v[v > threshold])
+     
+def display(v):
+    plt.plot(range(len(v)), v, color='b')
+    plt.show()
+    
 def tests():
     synchTheta = [0.7854, 1.5708, 2.3562]
     print synchrony(synchTheta)
@@ -126,6 +302,8 @@ def tests():
     old_ts = np.random.rand(100, 1) * math.pi * 2 - math.pi
 #     print synchrony(old_ts)
     
+    display( hhSim(0, 0, 0, 0, 1000, 0.1, 5) )
+    
     
 #     alpha = 0
 #     K = 0.02
@@ -134,23 +312,21 @@ def tests():
 #     for i in range(sim.shape[0]):
 #         print synchrony(sim[i,])
     
-    izSim = izhikevichSim(-65, 0, 5, 0.07, 0.25, -65, 8, 1000, 0.1)
-    izSim2 = izhikevichSim(-65, 0, 5, 0.1, 0.25, -65, 8, 1000, 0.1)
+#     izSim = izhikevichSim(-65, 0, 5, 0.07, 0.25, -65, 8, 1000, 0.1)
+#     izSim2 = izhikevichSim(-65, 0, 5, 0.1, 0.25, -65, 8, 1000, 0.1)
 #     print izSim[1]
 #     plt.plot(range(1000), izSim[0],color='red')
 #     plt.plot(range(1000), izSim2[0],color='blue')
-    print len(izSim[1][izSim[1] > 0])
+     
     
-    links = zip( np.random.randint(0, 100, 50).tolist(), np.random.randint(0, 100, 50).tolist() )
-    links = [(x, y) for (x, y) in links if x != y]
-    links = [(5,6), (6,4), (4,1), (5,4), (1,2), (1,3), (3,2)]
-    g = Graph(links)
+#     links = zip( np.random.randint(0, 100, 50).tolist(), np.random.randint(0, 100, 50).tolist() )
+#     links = [(x, y) for (x, y) in links if x != y]
+#     links = [(5,6), (6,4), (4,1), (5,4), (1,2), (1,3), (3,2)]
+#     g = Graph(links)
     
-    dt = np.linspace(-5, 5, 1000)
-    dw = map( weightUpdate, dt  )
-    plt.plot( dt, dw )
-    plt.show()
-
-    
+#     dt = np.linspace(-5, 5, 1000)
+#     dw = map( weightUpdate, dt  )
+#     plt.plot( dt, dw )
+#     plt.show()
+        
 tests()
-    
